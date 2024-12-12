f00e40e321d430955f5ffc05b0eceabcc9f774c2
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2024-12-13 09:13:53 +1000

    Day 3, Part 2

--- src/day03/src/main/java/com/neilwithdata/Day03.kt
+++ src/day03/src/main/java/com/neilwithdata/Day03.kt
@@ -8,6 +8,7 @@ fun main() {
 
     val regex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
 
+    // Part 1
     var total = 0
     for (line in input) {
         for (match in regex.findAll(line)) {
@@ -17,4 +18,26 @@ fun main() {
     }
 
     println(total)
+
+    // Part 2
+    val conditionalRegex = Regex("""mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""")
+    var conditionalTotal = 0
+    var enabled = true
+
+    for (line in input) {
+        for (match in conditionalRegex.findAll(line)) {
+            when (match.value) {
+                "do()" -> enabled = true
+                "don't()" -> enabled = false
+                else -> {
+                    if (enabled) {
+                        val (first, second) = match.destructured
+                        conditionalTotal += first.toInt() * second.toInt()
+                    }
+                }
+            }
+        }
+    }
+
+    println(conditionalTotal)
 }
