cf31d9b379ff0c220e024ea46aff31ab3f152253
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2024-12-20 08:51:42 +1000

    Day 5, Part 1

--- src/day05/src/main/java/com/neilwithdata/Day05.kt
+++ src/day05/src/main/java/com/neilwithdata/Day05.kt
@@ -6,8 +6,7 @@ fun main() {
     val lines = File("data/day05_input.txt")
         .readLines()
 
-    // convert the rules into a list of pairs
-    val rules = lines
+    val rules: List<Pair<Int, Int>> = lines
         .takeWhile { it.contains("|") }
         .map { line ->
             val first = line.substringBefore('|').toInt()
@@ -16,93 +15,41 @@ fun main() {
             first to second
         }
 
-    val chain = processRules(rules)
-
-    // test each of the rules against the chain we constructed
-    for (rule in rules) {
-        val firstIndex = chain.indexOf(rule.first)
-        val secondIndex = chain.indexOf(rule.second)
-
-        if (secondIndex < firstIndex) {
-            println("Rule $rule not captured properly in the chain")
-            return
-        }
-    }
-
-    // List<List<Int>>
-    val updates = lines
+    val updates: List<List<Int>> = lines
         .filter { it.contains(",") }
         .map { line ->
             line.split(",").map { it.toInt() }
         }
 
+    // for each update, build a map and then check each rule against this map
     var total = 0
     for (update in updates) {
-        var updateIndex = 0
-
-        for (c in chain) {
-            if (c == update[updateIndex]) {
-                updateIndex++
-                if (updateIndex >= update.size) {
-                    // Correct update. Following the rule chain sequentially we matched all the nums in update.
-                    total += update[update.size / 2]
-                    break
-                }
+        val updateMap = update
+            .withIndex()
+            .associate { (index, number) ->
+                number to index
             }
+
+        if (isUpdateValid(updateMap, rules)) {
+            total += update[update.size / 2]
         }
     }
 
     println(total)
 }
 
-fun processRules(rules: List<Pair<Int, Int>>): List<Int> {
-    val chains = mutableListOf<MutableList<Int>>()
-
+fun isUpdateValid(updateMap: Map<Int, Int>, rules: List<Pair<Int, Int>>): Boolean {
     for (rule in rules) {
-        println("Processing rule ${rule.first}|${rule.second}")
+        val firstIndex = updateMap[rule.first]
+        val secondIndex = updateMap[rule.second]
 
-        // Which existing chains contain the values from the rule
-        val first = chains.firstOrNull { it.contains(rule.first) }
-        val second = chains.firstOrNull { it.contains(rule.second) }
-
-        if (first == null && second == null) {
-            // there are no chains that contain these numbers - make a new chain
-            chains.add(mutableListOf(rule.first, rule.second))
-        } else if (first == null) {
-            // Add rule.first to start of the second
-            second!!.add(0, rule.first)
-        } else if (second == null) {
-            // Add rule.second to the end of first
-            first.add(rule.second)
-        } else {
-            if (first === second) {
-                // Re-order within same chain (if necessary)
-                val firstIndex = first.indexOf(rule.first)
-                val secondIndex = first.indexOf(rule.second)
-
-                if (firstIndex > secondIndex) {
-                    first.removeAt(firstIndex)
-                    first.add(secondIndex, rule.first)
-                }
-            } else {
-                // they're in different chains - add second to the end of first (and delete second)
-                chains.remove(second)
-                first.addAll(second)
+        if (firstIndex != null && secondIndex != null) {
+            if (firstIndex > secondIndex) {
+                return false // update fails this rule
             }
         }
-
-        displayChains(chains)
     }
 
-    // All rules always flatten neatly into a single chain
-    check(chains.size == 1)
-    return chains[0]
-}
-
-fun displayChains(chains: List<List<Int>>) {
-    println("Chains are now:")
-    for ((index, chain) in chains.withIndex()) {
-        println("$index: ${chain.joinToString()}")
-    }
-    println()
+    // All rules passed
+    return true
 }
