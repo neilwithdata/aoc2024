3d3f9f5c12e4fe9ab46f856f02f3c258f9dbc9d1
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2024-12-20 13:33:37 +1000

    Day 5, Part 2

--- src/day05/src/main/java/com/neilwithdata/Day05.kt
+++ src/day05/src/main/java/com/neilwithdata/Day05.kt
@@ -2,6 +2,86 @@ package com.neilwithdata
 
 import java.io.File
 
+class Graph {
+    private val nodes = mutableSetOf<Int>()
+
+    // number -> numbers it points to (comes before)
+    private val outgoingEdges = mutableMapOf<Int, MutableSet<Int>>()
+
+    // number -> numbers that point to it
+    private val incomingEdges = mutableMapOf<Int, MutableSet<Int>>()
+
+    fun add(from: Int, to: Int) {
+        nodes.add(from)
+        nodes.add(to)
+
+        // Update outgoing edges
+        if (from in outgoingEdges) {
+            outgoingEdges[from]!!.add(to)
+        } else {
+            outgoingEdges[from] = mutableSetOf(to)
+        }
+
+        // Update incoming edges
+        if (to in incomingEdges) {
+            incomingEdges[to]!!.add(from)
+        } else {
+            incomingEdges[to] = mutableSetOf(from)
+        }
+    }
+
+    private fun removeEdge(from: Int, to: Int) {
+        outgoingEdges[from]!!.remove(to)
+        incomingEdges[to]!!.remove(from)
+    }
+
+    /**
+    ~~ KAHN'S ALGORITHM - TOPOLOGICAL SORT ~~
+    L ← Empty list that will contain the sorted elements
+    S ← Set of all nodes with no incoming edge
+
+    while S is not empty do
+    remove a node n from S
+    add n to L
+    for each node m with an edge e from n to m do
+    remove edge e from the graph
+    if m has no other incoming edges then
+    insert m into S
+
+    if graph has edges then
+    return error   (graph has at least one cycle)
+    else
+    return L   (a topologically sorted order)
+     **/
+    fun topologicalSort(): List<Int> {
+        val sortedList = mutableListOf<Int>()
+
+        val noIncoming = mutableSetOf<Int>()
+        noIncoming.addAll(nodes.filter { it !in incomingEdges })
+
+        while (noIncoming.isNotEmpty()) {
+            val n = noIncoming.first()
+            noIncoming.remove(n)
+
+            sortedList.add(n)
+
+            // find all the nodes m where n was incoming
+            val connected = (outgoingEdges[n])?.toSet() ?: emptySet()
+            for (m in connected) {
+                // remove the edge from n -> m
+                removeEdge(n, m)
+
+                // does m have any other incoming edges?
+                if (incomingEdges[m].isNullOrEmpty()) {
+                    noIncoming.add(m)
+                }
+            }
+        }
+
+        return sortedList
+    }
+}
+
 fun main() {
     val lines = File("data/day05_input.txt")
         .readLines()
@@ -21,8 +101,8 @@ fun main() {
             line.split(",").map { it.toInt() }
         }
 
-    // for each update, build a map and then check each rule against this map
-    var total = 0
+    var correctTotal = 0
+    var incorrectTotal = 0
     for (update in updates) {
         val updateMap = update
             .withIndex()
@@ -31,11 +111,30 @@ fun main() {
             }
 
         if (isUpdateValid(updateMap, rules)) {
-            total += update[update.size / 2]
+            correctTotal += update[update.size / 2]
+        } else {
+            val graph = Graph().apply {
+                // Build a graph using only those rules contained in the update
+                for (rule in rules) {
+                    if (rule.first in update && rule.second in update) {
+                        add(rule.first, rule.second)
+                    }
+                }
+            }
+
+            // Requires re-ordering - Part 2
+            val reordered = graph.topologicalSort()
+                .filter { it in update }
+
+            incorrectTotal += reordered[reordered.size / 2]
         }
     }
 
-    println(total)
+    // Part 1
+    println(correctTotal)
+
+    // Part 2
+    println(incorrectTotal)
 }
 
 fun isUpdateValid(updateMap: Map<Int, Int>, rules: List<Pair<Int, Int>>): Boolean {
