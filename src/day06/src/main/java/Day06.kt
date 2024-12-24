aef0812171375dd9795175e371783f5e20772f35
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2024-12-24 13:12:02 +1000

    Day 6, Part 2

--- src/day06/src/main/java/Day06.kt
+++ src/day06/src/main/java/Day06.kt
@@ -4,8 +4,27 @@ enum class Direction(val vector: Pair<Int, Int>) {
     NORTH(Pair(-1, 0)),
     EAST(Pair(0, 1)),
     SOUTH(Pair(1, 0)),
-    WEST(Pair(0, -1)),
+    WEST(Pair(0, -1));
+
+    override fun toString(): String =
+        when (this) {
+            NORTH -> "↑"
+            EAST -> "→"
+            SOUTH -> "↓"
+            WEST -> "←"
+        }
 }
+
+// Position + facing direction of guard
+data class Transform(
+    val position: Pair<Int, Int>,
+    val direction: Direction
+)
+
+data class PatrolRoute(
+    val transforms: Set<Transform>,
+    val didExit: Boolean
+)
 
 operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
     Pair(this.first + other.first, this.second + other.second)
@@ -13,6 +32,13 @@ operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
 class Grid(input: List<String>) {
     private val numRows = input.size
     private val numCols = input[0].length
+
+    companion object {
+        private const val START = '^'
+        private const val OBSTRUCTION = 'O'
+        private const val OBSTACLE = '#'
+        private const val CLEAR = '.'
+    }
 
     private val values =
         Array(numRows) { row ->
@@ -43,22 +69,74 @@ class Grid(input: List<String>) {
     private fun withinBounds(row: Int, col: Int): Boolean =
         (row in 0 until numRows) && (col in 0 until numCols)
 
-    // Guard patrol - starting from ^ patrol and return # distinct positions visited
-    fun patrol(): Int {
-        val visited = mutableSetOf<Pair<Int, Int>>()
+    // Move the obstruction to the specified location
+    private fun updateObstruction(row: Int, col: Int) {
+        // Clear out the previous obstruction
+        val location = indexOf(OBSTRUCTION)
+        if (location != Pair(-1, -1)) {
+            values[location.first][location.second] = CLEAR
+        }
+
+        // Set the new obstruction location
+        values[row][col] = OBSTRUCTION
+    }
+
+    // Count of number of unique positions visited (Part 1)
+    fun uniquePositions(): Int =
+        patrol().transforms.map { it.position }.toSet().count()
+
+    // Part 2
+    fun loopCount(): Int {
+        // Perform a default patrol to get all the transforms (default scenario)
+        val patrolRoute = patrol()
+
+        val obstructionsThatCauseLoops = mutableSetOf<Pair<Int, Int>>()
+
+        // for each transform, set an obstruction in the next position and evaluate the patrol
+        for (transform in patrolRoute.transforms) {
+            val next = transform.position + transform.direction.vector
+
+            if (!withinBounds(next) || charAt(next) == OBSTACLE || charAt(next) == START) {
+                // Nothing to do - can't put an obstacle in this location
+            } else {
+                // Put the obstruction in the next position and evaluate to see if this causes a loop
+                updateObstruction(next.first, next.second)
+                val newRoute = patrol()
+
+                if (!newRoute.didExit) {
+                    obstructionsThatCauseLoops.add(next)
+                }
+            }
+        }
+
+        return obstructionsThatCauseLoops.size
+    }
+
+    // Guard patrol - starting from ^ perform patrol
+    private fun patrol(): PatrolRoute {
+        val visited = mutableSetOf<Transform>()
         val directions = Direction.entries.toList()
 
-        var position = indexOf('^')
+        var position = indexOf(START)
         var direction = Direction.NORTH
 
         while (true) {
-            visited.add(position)
+            val currentTransform = Transform(position, direction)
+            if (currentTransform in visited) {
+                // We've been here before so we're stuck in a loop
+                return PatrolRoute(visited, didExit = false)
+            } else {
+                visited.add(currentTransform)
+            }
 
             // Move to next location
             val next = position + direction.vector
             if (!withinBounds(next)) {
-                return visited.count()
-            } else if (charAt(next) == '#') {
+                return PatrolRoute(
+                    visited,
+                    didExit = true
+                )
+            } else if (charAt(next) == OBSTACLE || charAt(next) == OBSTRUCTION) {
                 // Turn 90 degrees
                 direction = directions[(directions.indexOf(direction) + 1) % 4]
             } else {
@@ -73,7 +151,12 @@ fun main() {
         .readLines()
 
     val grid = Grid(input)
-    val visited = grid.patrol()
 
-    println(visited)
+    // Part 1
+    val visitedCount = grid.uniquePositions()
+    println(visitedCount)
+
+    // Part 2
+    val loopCount = grid.loopCount()
+    println(loopCount)
 }
