6c558636fb907acaf46fb94438cd5478cad6d927
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2025-01-28 09:10:16 +1000

    Day 11, Part 2

--- src/day11/src/main/java/Day11.kt
+++ src/day11/src/main/java/Day11.kt
@@ -1,18 +1,64 @@
 import java.io.File
 
 fun main() {
-    var stones = File("data/day11_input.txt")
+    val stones = File("data/day11_input.txt")
         .readLines()[0]
         .split(" ")
         .map {
             it.toLong()
         }
 
+    // Part 1
+    var first = stones
     repeat(25) {
-        stones = blink(stones)
+        first = blink(first)
     }
 
-    println(stones.size)
+    println(first.size)
+
+    // Part 2
+    var counts = stones.groupingBy { it }
+        .eachCount()
+        .mapValues { it.value.toLong() }
+
+    repeat(75) {
+        counts = updateCounts(counts)
+    }
+
+    println(counts.values.sum())
+}
+
+fun updateCounts(counts: Map<Long, Long>): Map<Long, Long> {
+    val updated = mutableMapOf<Long, Long>()
+
+    fun increaseCount(stone: Long, n: Long) {
+        updated[stone] = updated.getOrDefault(stone, 0) + n
+    }
+
+    for ((stone, count) in counts) {
+        val str = stone.toString()
+        val len = str.length
+
+        when {
+            stone == 0L -> {
+                increaseCount(1L, count)
+            }
+
+            len % 2 == 0 -> {
+                val leftHalf = str.substring(0 until len / 2)
+                val rightHalf = str.substring(len / 2 until len)
+
+                increaseCount(leftHalf.toLong(), count)
+                increaseCount(rightHalf.toLong(), count)
+            }
+
+            else -> {
+                increaseCount(stone * 2024, count)
+            }
+        }
+    }
+
+    return updated.toMap()
 }
 
 fun blink(stones: List<Long>): List<Long> {
